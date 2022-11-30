de1efec9c77ad9c43ee5f18b3fe2c29a2de60f6c
jgriswo3 <memoriesofthedelusionalpig@protonmail.com>
2022-11-30 03:33:39 -0500

    implement game manager and player ship

---
+++ SGD268FinalProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+    //The following region are sound effects.
+    #region
+    public AudioClip startGame;
+    public AudioClip playerGun;
+    public AudioClip enemyGun;
+    public AudioClip playerBomb;
+    public AudioClip playerHit;
+    public AudioClip enemyExplosion;
+    public AudioClip dryTimer;
+    #endregion
+    void Awake()
+    {
+        if(!gm)
+            gm = this;
+        else
+            Destroy(this);
+    }
+    public Text scoreText;
+    //The following region contains functions for playing audio clips.
+    #region
+    public void StartGame()
+    {
+        AudioSource.PlayClipAtPoint(startGame, Camera.main.transform.position);
+    }
+    public void PlayerGun()
+    {
+        AudioSource.PlayClipAtPoint(playerGun, Camera.main.transform.position);
+    }
+    public void EnemyGun()
+    {
+        AudioSource.PlayClipAtPoint(enemyGun, Camera.main.transform.position);
+    }
+    public void PlayerBomb()
+    {
+        AudioSource.PlayClipAtPoint(playerBomb, Camera.main.transform.position);
+    }
+    public void PlayerHit()
+    {
+        AudioSource.PlayClipAtPoint(playerHit, Camera.main.transform.position);
+    }
+    public void EnemyExplosion()
+    {
+        AudioSource.PlayClipAtPoint(enemyExplosion, Camera.main.transform.position);
+    }
+    public void DryTimer()
+    {
+        AudioSource.PlayClipAtPoint(dryTimer, Camera.main.transform.position);
+    }
+    #endregion
+}
